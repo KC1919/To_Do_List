15d362377c227d69a0f0aa5110a8192a8fcd0172
KC1919 <kunal.chandra1900@gmail.com>
2021-06-16 22:53:51 +0530

    to-do-list app

---
+++ src/components/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import Item from "./ToDoItem";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
+
+  function deleteItem(id) {
+    setItems((previousItems) => {
+      return previousItems.filter((value, index) => {
+        return id !== index;
+      });
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem, index) => (
+            <Item
+              key={index}
+              id={index}
+              onChecked={deleteItem}
+              item={todoItem}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
